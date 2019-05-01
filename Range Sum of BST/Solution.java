8b59dab0f5efa349d09e6f7f2189a560c46a7f9c
Deon Liang <deonyuhaoliang@gmail.com>
2019-04-30 22:26:57 -0700

    Java solution fix does not traverse unnecessary nodes

--- Range Sum of BST/Solution.java
+++ Range Sum of BST/Solution.java
@@ -12,10 +12,15 @@ class Solution {
     public int rangeSumBST(TreeNode root, int L, int R) {
         
         if(root==null) return 0;
-        else if(root.val<L||root.val>R){
-            root.val = 0;
+        else{ 
+            if(root.val<L){
+                return rangeSumBST(root.right,L,R);
+            }else if(root.val>R){
+                return rangeSumBST(root.left,L,R);
+            }else{
+                return root.val + rangeSumBST(root.left,L,R) + rangeSumBST(root.right,L,R);
+            }
         }
-        return root.val + rangeSumBST(root.left,L,R) + rangeSumBST(root.right,L,R); 
     }
     
 }
